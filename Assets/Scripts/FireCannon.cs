083f835f7363de25b221c8ce910814418d90d696
MichaelAng <ma87834n@pace.edu>
2014-02-14 15:08:51 -0500

    Add Cannon Idle and Fire Animation off a trigger

---
+++ Assets/Scripts/FireCannon.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireCannon : MonoBehaviour{
+
+	//creates an Animator
+	Animator animCannonFire;
+	//public Texture button;
+	public GUIStyle buttonStyle;
+	//public GUILayout button;
+
+
+	// Use this for initialization
+	void Start () {
+		//gets the Animator Component
+		animCannonFire = GetComponent<Animator>();
+	}
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnGUI () {	
+		GUI.contentColor = Color.white;
+				if(GUI.Button(new Rect(393,379,67,22),"",buttonStyle)){
+				//if (GUI.Button(new Rect(393,379,67,22),button)){
+					animCannonFire.SetTrigger("cannonFireTrigger");
+				}
+		}
+
+}
