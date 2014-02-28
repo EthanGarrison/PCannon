d8606866ea4ff31523129429ce3fb3afc4814ec5
MichaelAng <ma87834n@pace.edu>
2014-02-28 00:35:55 -0500

    Move cannon and add kill logic.

--- Assets/Scripts/FireCannon.cs
+++ Assets/Scripts/FireCannon.cs
@@ -8,25 +8,37 @@ public class FireCannon : MonoBehaviour{
 	//public Texture button;
 	public GUIStyle buttonStyle;
 	public Rigidbody2D ball;
+	public float ballPositionX;
+	public float ballPositionY;
+	public float ballVelocity;
+	public float ballAngle;
 
 
 	// Use this for initialization
-	void Start () {
+//	void Start () {
 		//gets the Animator Component
-		animCannonFire = GetComponent<Animator>();
+//		animCannonFire = GetComponent<Animator>();
 
-	}
+//	}
 	// Update is called once per frame
-	void Update () {
+/*	void Update () {
 	}
 
 	void OnGUI () {	
 		GUI.contentColor = Color.white;
-				if(GUI.Button(new Rect(393,379,67,22),"",buttonStyle)){
-				//if (GUI.Button(new Rect(393,379,67,22),button)){
-					animCannonFire.SetTrigger("cannonFireTrigger");
-					ball.AddForce (new Vector2 (80,10));
+		if(GUI.Button(new Rect(393,379,67,22),"",buttonStyle)){
+			resetBall();
+			fireBall();
 		}
-		}
-		
+	}
+
+	void resetBall(){
+		ball.velocity = new Vector3 (0,0,0);
+		ball.transform.position = new Vector3(ballPositionX, ballPositionY, 0.0f);	
+	}
+
+	void fireBall(){
+		animCannonFire.SetTrigger("cannonFireTrigger");
+		ball.AddForce (new Vector2 (ballVelocity,ballAngle));
+	}*/
 }
