0b2c5e49784248cbcb8bf0d8dd46336f539d8f7a
MichaelAng <ma87834n@pace.edu>
2014-03-17 12:49:30 -0400

    Create Animation Assets.

---
+++ Assets/Scripts/BallFlightPath.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallFlightPath : MonoBehaviour {
+
+	Animator anim;
+	
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+
+		//AnimatorStateInfo shortFire = Animat
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){	
+			anim.SetTrigger("Short");
+		}
+
+	}
+
+
+}
