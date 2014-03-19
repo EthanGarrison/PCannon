6eb89916f451d7489ebfd0d8bad1257ff827ae33
MichaelAng <ma87834n@pace.edu>
2014-03-19 16:46:45 -0400

    Refactor BallFlightPath.

--- Assets/Scripts/BallFlightPath.cs
+++ Assets/Scripts/BallFlightPath.cs
@@ -2,17 +2,10 @@
 using System.Collections;
 
 public class BallFlightPath : MonoBehaviour {
-
 	Animator anim;
 	
-	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 
 	public void PlayShort(){		
