fa652e8f45b6472d98aa97886af6264ac716db61
MichaelAng <ma87834n@pace.edu>
2014-03-20 18:48:41 -0400

    Changed GameStat and GameOver class to create rect one time.

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -4,10 +4,20 @@ using System.Collections;
 public class GameOver : MonoBehaviour {
 
 	public static bool gameStatDisplayUp = false;
+	private Rect statDisplay, box, label, button;
 
 	// Use this for initialization
 	void Start () {
 		gameStatDisplayUp = true;
+		statDisplay	= new Rect (
+			GameMaster.nativeWidth* (1.5f/32), 
+			GameMaster.nativeHeight* (1.5f/20), 
+			GameMaster.nativeWidth * (29f/32), 
+			GameMaster.nativeHeight * (17f/20)
+		);
+		box = new Rect (0,0,statDisplay.width,statDisplay.height);
+		label = new  Rect(10,15,statDisplay.width,statDisplay.height);
+		button = new Rect (statDisplay.width/2f-40f,statDisplay.height-35f,80f,30f);
 	}
 	
 	// Update is called once per frame
@@ -17,21 +27,15 @@ public class GameOver : MonoBehaviour {
 
 	void OnGUI(){
 		GameMaster.AutoResize(GameMaster.nativeWidth,GameMaster.nativeHeight);
-		Rect StatDisplay = new Rect (
-			GameMaster.nativeWidth/14, 
-			GameMaster.nativeHeight/14, 
-			GameMaster.nativeWidth * (12f/14), 
-			GameMaster.nativeHeight * (12f/14)
-		);
 
-		GUI.BeginGroup (StatDisplay);
-			GUI.Box (new Rect (0,0,StatDisplay.width,StatDisplay.height), " GameOver");
-			GUI.Label(new Rect(10,15,StatDisplay.width,StatDisplay.height),
+		GUI.BeginGroup (statDisplay);
+			GUI.Box (box, " GameOver");
+			GUI.Label(label,
 			"Highest Level"+ " : " + (GameMaster.level-1) + "\n" +
 			"Remaining FuseLife: " + 0 + "\n" +
 			"Total Points : " + GameMaster.point + "\n");
 
-			if(GUI.Button (new Rect (StatDisplay.width/2-40,StatDisplay.height-35,80,30), "OK")){
+			if(GUI.Button (button, "OK")){
 					gameStatDisplayUp = false;
 					Object.Destroy(gameObject, 0f);
 					//Go to title screen
--- Assets/Scripts/GameStat.cs
+++ Assets/Scripts/GameStat.cs
@@ -3,11 +3,21 @@ using System.Collections;
 
 public class GameStat : MonoBehaviour {
 
-	public static bool gameStatDisplayUp = false;
+	public static bool gameStatDisplayUp = false;	
+	private Rect statDisplay, box, label, button;
 
 	// Use this for initialization
 	void Start () {
 		gameStatDisplayUp = true;
+		statDisplay	= new Rect (
+			GameMaster.nativeWidth* (1.5f/32), 
+			GameMaster.nativeHeight* (1.5f/20), 
+			GameMaster.nativeWidth * (29f/32), 
+			GameMaster.nativeHeight * (17f/20)
+		);
+		box = new Rect (0,0,statDisplay.width,statDisplay.height);
+		label = new  Rect(10,15,statDisplay.width,statDisplay.height);
+		button = new Rect (statDisplay.width/2f-40f,statDisplay.height-35f,80f,30f);
 	}
 	
 	// Update is called once per frame
@@ -17,21 +27,15 @@ public class GameStat : MonoBehaviour {
 
 	void OnGUI(){
 		GameMaster.AutoResize(GameMaster.nativeWidth,GameMaster.nativeHeight);
-		Rect StatDisplay = new Rect (
-			GameMaster.nativeWidth/14, 
-			GameMaster.nativeHeight/14, 
-			GameMaster.nativeWidth * (12f/14), 
-			GameMaster.nativeHeight * (12f/14)
-		);
-
-		GUI.BeginGroup (StatDisplay);
-			GUI.Box (new Rect (0,0,StatDisplay.width,StatDisplay.height), " LEVEL");
-			GUI.Label(new Rect(10,15,StatDisplay.width,StatDisplay.height),
+		
+		GUI.BeginGroup (statDisplay);
+			GUI.Box (box, " LEVEL");
+			GUI.Label(label,
 			"Next Level"+ " : " + GameMaster.level + "\n" +
 			"Remaining FuseLife: " + GameMaster.FuseLife + "\n" +
 			"Total Points : " + GameMaster.point + "\n");
 
-			if(GUI.Button (new Rect (StatDisplay.width/2-40,StatDisplay.height-35,80,30), "OK")){
+			if(GUI.Button (button, "OK")){
 					gameStatDisplayUp = false;
 					Object.Destroy(gameObject, 0f);
 			}
