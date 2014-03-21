a69a0924a439016462759c0482f4ae61b3a14984
EthanGarrison <emrys94@gmail.com>
2014-03-21 19:39:14 -0400

    Added script to transition from start screen to game screen

---
+++ Assets/Scripts/TitleScreenScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenScript : MonoBehaviour {
+
+	Rect startRect, exitRect;
+
+	// Use this for initialization
+	void Start () {
+	
+		startRect = new Rect(0,0,100,100);
+
+		exitRect = new Rect(100,100,100,100);
+
+	}
+
+	void OnGUI() {
+
+		if(GUI.Button(startRect, "Start Game")){
+			Application.LoadLevel(1);
+		}
+
+		if(GUI.Button(exitRect, "Exit Game")){
+			Application.Quit();
+		}
+	}
+}
