1f234a46b00b69693a5ab4578fc7b0e15e2a7c8a
EthanGarrison <emrys94@gmail.com>
2014-04-23 18:26:30 -0400

    Updated the Game Over Screen and Game Stat Screen to the proper sizes

--- Assets/Scripts/GameStat.cs
+++ Assets/Scripts/GameStat.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class GameStat : MonoBehaviour {
 
 	public static bool gameStatDisplayUp = false;	
-	private Rect statDisplay, label, button;
+	private Rect statDisplay,  label, button, gameOverDisplay;
 	public GUIStyle fontLabel,fontButton,gameStatStyle;
 	// Use this for initialization
 	void Start () {
@@ -14,6 +14,12 @@ public class GameStat : MonoBehaviour {
 			GameMaster.nativeHeight* (1.5f/20), 
 			GameMaster.nativeWidth * (16f/32), 
 			GameMaster.nativeHeight * (14f/20)
+		);
+		gameOverDisplay	= new Rect (
+			GameMaster.nativeWidth* (1.5f/32), 
+			GameMaster.nativeHeight* (1.5f/20), 
+			GameMaster.nativeWidth * (29f/32), 
+			GameMaster.nativeHeight * (17f/20)
 		);
 		label = new  Rect(10,15,statDisplay.width,statDisplay.height);
 		button = new Rect (statDisplay.width/2f-100f,statDisplay.height-150f,200f,100f);
@@ -26,27 +32,32 @@ public class GameStat : MonoBehaviour {
 
 	void OnGUI(){
 		GameMaster.AutoResize(GameMaster.nativeWidth,GameMaster.nativeHeight);
-		GUI.BeginGroup (statDisplay, gameStatStyle);
 			if(GameMaster.FuseLife > 0){
-				GUI.Label(label,
+				GUI.BeginGroup (statDisplay, gameStatStyle);
+				GUI.Label(new  Rect(10,15,statDisplay.width,statDisplay.height),
 				"Victory! \n" +
 				"Next Level"+ " : " + GameMaster.level + "\n" +
 				"FuseLife: " + GameMaster.FuseLife + "\n" +
 				"Total Points : " + GameMaster.point + "\n",fontLabel);
+				
+				if(GUI.Button (new Rect (statDisplay.width/2f-100f,statDisplay.height-150f,200f,100f), "OK",fontButton)){
+					gameStatDisplayUp = false;
+					Object.Destroy(gameObject, 0f);
+				}
 			}else{
-				GUI.Label(label,
+				GUI.BeginGroup (gameOverDisplay, gameStatStyle);
+				GUI.Label(new  Rect(10,15,gameOverDisplay.width,gameOverDisplay.height),
 				"Game Over \n" +
 				"Highest Level"+ " : " + (GameMaster.level-1) + "\n" +
 				"FuseLife: " + 0 + "\n" +
 				"Total Points : " + GameMaster.point + "\n", fontLabel);
+				
+				if(GUI.Button (new Rect (gameOverDisplay.width/2f-100f,gameOverDisplay.height-150f,200f,100f), "OK",fontButton)){
+					gameStatDisplayUp = false;
+					Object.Destroy(gameObject, 0f);
+					Application.LoadLevel(0);
+				}			
 			}
-
-			if(GUI.Button (button, "OK",fontButton)){
-				gameStatDisplayUp = false;
-				Object.Destroy(gameObject, 0f);
-				if(GameMaster.FuseLife <= 0) Debug.Log("END GAME!!!");
-			}
-
 		GUI.EndGroup ();
 	}
 }
